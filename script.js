d11fd1c8fceb7c971a2c283dce9ee0d16378b434
saddammaratouq <>
2021-06-03 18:55:39 +0000

    add feathers js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var userName = prompt("What's your name ? ");
+
+document.write("Your name is " + userName)
+
+
+var useAge = prompt("What's your age ? ");
+
+document.write(", your age is  " + useAge)
+alert("nice to meet you" + " " + userName );
+
+
