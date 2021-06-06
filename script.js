4ebda3b72b5e47518ce129fff79775ed547242db
saddammq <>
2021-06-06 15:49:11 +0000

    while add

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 
 
-var userName = prompt("What's your name ? ");
+// var userName = prompt("What's your name ? ");
 
-document.write("Your name is " + userName)
+// document.write("Your name is " + userName)
 
 
-var useAge = prompt("What's your age ? ");
+// var useAge = prompt("What's your age ? ");
 
-document.write(", your age is  " + useAge)
-alert("nice to meet you" + " " + userName );
+// document.write(", your age is  " + useAge)
+// alert("nice to meet you" + " " + userName);
+
+
+
+for (i = 0; i < 2 ; i++) {
+  alert("hi")
+}
+
+
+var favmovies = prompt(" whats your fav movies : 1917 , host " );
+
+while(favmovies != "1917" && favmovies != "host"){
+
+   favmovies = prompt("plz enter 1917 , host");
+  }
+  alert("   welcome dear &  💕  ")
+  
+  document.write(" <img src= 'https://app.retargetbot.com/upload/comboposter/2/2_image_1582136788872399.jpg'/> ")
+
+
+var x=prompt("how many heart give page  ")
+
+var i=0 
+while(i < x){
+
+document.write(" <img src= 'https://images.assetsdelivery.com/compings_v2/marsono/marsono1810/marsono181000489.jpg'/> ")
+i++
+ 
+
+
+
+}
+
+
+
+
+
 
 
