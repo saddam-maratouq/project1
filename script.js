e90d3cc48809fd77846c8b4f5bcd38e7da7f8077
saddammq <>
2021-06-06 20:29:45 +0000

    add func

--- script.js
+++ script.js
@@ -11,35 +11,44 @@
 // alert("nice to meet you" + " " + userName);
 
 
-
+var i;
 for (i = 0; i < 2 ; i++) {
   alert("hi")
 }
 
+favmovies(); 
+
+
 
 var favmovies = prompt(" whats your fav movies : 1917 , host " );
+
+function favmovies() {
 
 while(favmovies != "1917" && favmovies != "host"){
 
    favmovies = prompt("plz enter 1917 , host");
   }
-  alert("   welcome dear &  💕  ")
+
+  alert("   welcome dear &  💕  ") 
+
+
   
   document.write(" <img src= 'https://app.retargetbot.com/upload/comboposter/2/2_image_1582136788872399.jpg'/> ")
-
-
+}
+var y=""
 var x=prompt("how many heart give page  ")
 
 var i=0 
 while(i < x){
 
-document.write(" <img src= 'https://images.assetsdelivery.com/compings_v2/marsono/marsono1810/marsono181000489.jpg'/> ")
+y=y+" <img src= 'https://images.assetsdelivery.com/compings_v2/marsono/marsono1810/marsono181000489.jpg'/> "
 i++
- 
+}document.write(y)
 
 
 
-}
+
+
 
 
 
